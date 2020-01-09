2bf95c3dd45f18fe3481cb75b905eeb5c6d66f55
testandbench <59679689+testandbench@users.noreply.github.com>
2020-01-09 11:49:13 +0530

    Add files via upload

---
+++ Multi_Dimensional_Array.c
@@ -0,0 +1,18 @@
+//Done on 9/1/2020
+#include<stdio.h>
+void main()
+{
+
+    int i,j;
+    int arr[2][3];
+    printf("Enter the values of Array");
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+  printf("%3d ",a[i][j]);
+
+}
