452ad025c06643e6a1f34141ed332b091d0b6bcd
testandbench <59679689+testandbench@users.noreply.github.com>
2020-01-09 16:38:17 +0530

    Add files via upload

---
+++ continue.c
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+    printf("the loop produces output as:\n");
+    for(int i=1;i<=10;i++)
+    {
+        if((i%3)==0)
+        {
+            continue;
+        }
+        else{
+            printf("%d",i);
+        }
+    }
+}
